e7c4d2fed942031f8dde38a3177d63dda5a91ab7
ajinkyabele <ajinkyabele163@gmail.com>
2025-02-20 10:42:03 +0000

    feat: Allows external access for port 3000

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    css: {
-      devSourcemap: false
-    }
-})
+ plugins: [react()],
+ base: './',
+ server: {
+   host: '0.0.0.0', // Allows external access
+   port: 3000,      // Forces port 3000
+   strictPort: true // Ensures it doesn't change ports
+ },
+ css: {
+   devSourcemap: false,
+ },
+});
